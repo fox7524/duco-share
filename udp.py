56ce1fe2e1f643a8c70eaffa35d5a8647a9f0899
callisto1232 <ahmet_ka1232@hotmail.com>
2026-05-29 19:23:44 +0300

    a

--- udp.py
+++ udp.py
@@ -1,30 +1,31 @@
 import socket
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "kayrayı seviyorum"
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005
+# MESSAGE = b"kayrayi seviyorum"
+# will be added to main.py
 
 class SEND:
-    def __init__(self):
-        print("UDP target IP: %s" % UDP_IP)
-        print("UDP target port: %s" % UDP_PORT)
-        print("message: %s" % MESSAGE)
+    def __init__(self, UDP_IP, UDP_PORT, MESSAGE): # sends the dedicated message to the dedicated ip and port
+        # print("UDP target IP: %s" % UDP_IP)
+        # print("UDP target port: %s" % UDP_PORT)
+        # print("message: %s" % MESSAGE)
         
         sock = socket.socket(socket.AF_INET, # Internet
                              socket.SOCK_DGRAM) # UDP
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 class RECEIVE:
-    def __init__(self):
+    def __init__(self, UDP_IP, UDP_PORT, MESSAGE):
         sock = socket.socket(socket.AF_INET, # Internet
                              socket.SOCK_DGRAM) # UDP
         sock.bind((UDP_IP, UDP_PORT))
         
         while True:
             data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-            print("received message: %s" % data)
+            return data
 
 if __name__ == "__main__":
     if input("s mi r mi \n") == "s":
-        print(SEND)
+        print(SEND(UDP_IP, UDP_PORT, MESSAGE))
     else:
-        print(RECEIVE)
+        print(RECEIVE(UDP_IP, UDP_PORT, MESSAGE))
