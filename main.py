5784a5acca260ad9b755521ecc3e94f6e7349cc2
fox <foxkyra1453@gmail.com>
2026-05-29 19:17:49 +0300

    .

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from udp import RECEIVE
 password = 42
 
 if __name__ == "__main__":
+    message = input("Şifrelenecek mesajı girin: ")
+    encryption = Encryption(password)
+    encryipted_message = encryption.encrypt(message)
+    print(f"Şifreli hali: {encryipted_message}")
+    decryption = Decryption(password)
+    decryipted_message = decryption.decrypt(encryipted_message)
+    print(f"Çözülmüş hali: {decryipted_message}")
+    if input("s mi r mi \n") == "s":
+        print(SEND)
+    else:
+        print(RECEIVE)
+        
