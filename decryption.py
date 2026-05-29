dbd26d2561c2f195aadee7a87439f643d8885920
fox <foxkyra1453@gmail.com>
2026-05-29 19:07:22 +0300

    .

--- decryption.py
+++ decryption.py
@@ -1,6 +1,13 @@
 password = 42
+class Decryption:
+    def __init__(self, password):
+        self.password = password
+
+    def decrypt(self, encryipted_message):
+        return "".join([chr(ord(c) ^ self.password) for c in encryipted_message])
+
+# Kullanım
+decryption = Decryption(password)
 encryipted_message = input("Şifreli mesajı girin: ")
-
-decryipted_message = "".join([chr(ord(c) ^ password) for c in encryipted_message])
-
+decryipted_message = decryption.decrypt(encryipted_message)
 print(f"Çözülmüş hali: {decryipted_message}")
--- encryption.py
+++ encryption.py
@@ -1,8 +1,16 @@
 password = 42
+class Encryption:
+    def __init__(self, password):
+        self.password = password
+
+    def encrypt(self, message):
+        return "".join([chr(ord(c) ^ self.password) for c in message])
+
+# Kullanım
+encryption = Encryption(password)
 message = input("Şifrelenecek mesajı girin: ")
 
 # Şifreleme (Tek Satır)
-encryipted_message = "".join([chr(ord(c) ^ password) for c in message])
+encryipted_message = encryption.encrypt(message)
 
 print(f"Şifreli hali: {encryipted_message}")
-
