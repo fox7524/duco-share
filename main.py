6bcd8dd6d24c9df27741e281735c45436dae9789
callisto1232 <ahmet_ka1232@hotmail.com>
2026-05-29 19:44:17 +0300

    a

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ UDP_IP = "127.0.0.1"
 MESSAGE = b"kayrayi seviyorum"
 password = 42
 channel = input("kanal seçiminizi yapın amk: ")
-UDP_PORT = channel
+UDP_PORT = int(channel)
 
 # print(SEND(UDP_IP, UDP_PORT, MESSAGE))
 # print(RECEIVE(UDP_IP, UDP_PORT, MESSAGE))
+
+if input("s mi r mi: ") == "s":
+    print(SEND(UDP_IP, UDP_PORT, MESSAGE))
+else:
+    print(RECEIVE(UDP_IP, UDP_port, MESSAGE))
